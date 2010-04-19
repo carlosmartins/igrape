2c938e166bfe7fdfed51feb0890017814e69f18a
Thiago Avelino <avelinoo@me.com>
2010-04-18 23:42:28 -0300

    Upgrade version

--- app/lib/dataElement.php
+++
@@ -1,7 +0,0 @@
-<hr/>
-<h1>Hey, this is my dataElement</h1>
-<p>This file was rendered from dataElement.php, which received the <i>$data</i> variable via parameters</p>
-<p><b>Name is: </b><?=$data['name']?></p>
-<p><b>Age is: </b><?=$data['age']?></p>
-<p><b>Color is: </b><?=$data['color']?></p>
-<hr/>
--- app/views/cart/checkout.php
+++
@@ -1,4 +0,0 @@
-<form action="<?=url('/cart/confirm') ?>" method="post">
-<p>Please type in your email address: <input type="text" name="email"/></p>
-<p><input type="submit" value="Confirm checkout"></p>
-</form>
